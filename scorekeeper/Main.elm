c3334a9fe68cfeabdf6223a0e536e76b924d379a
pnaranja <paul.naranja@gmail.com>
2016-07-30 02:14:37 -0700

    Almost finished Form Section.  Need to finish EditPlayer Update logic

--- scorekeeper/Main.elm
+++ scorekeeper/Main.elm
@@ -1,6 +1,131 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.App as App
+import String
 
-main : Html msg
-main = text "Hello ScoreKeeper!"
+
+--Model
+
+
+type Mode a
+    = EditPlayer a
+    | AddPlayer
+
+type alias PlayerId = Int
+
+
+type alias Model =
+    { players : List Player
+    , name : String
+    , mode : Mode PlayerId
+    , plays : List Play
+    }
+
+
+type alias Player =
+    { id : Int
+    , name : String
+    , totalpoints : Int
+    }
+
+
+type alias Play =
+    { id : Int
+    , playerId : PlayerId
+    , name : Int
+    , points : Int
+    }
+
+
+initModel : Model
+initModel =
+    { players = []
+    , name = ""
+    , mode = AddPlayer
+    , plays = []
+    }
+
+
+
+--Update
+
+
+type Pts
+    = Int
+
+
+type Msg
+    = Edit Player
+    | Score Player Pts
+    | Input String
+    | Save
+    | Cancel
+    | Delete Play
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Input name ->
+            { model | name = name }
+
+        Cancel ->
+            { model | name = "", mode = AddPlayer }
+
+        Save ->
+            if (String.isEmpty model.name) then
+                model
+            else
+                saveModel model
+
+        _ ->
+            model
+
+
+saveModel : Model -> Model
+saveModel model =
+    case model.mode of
+        AddPlayer ->
+            { model
+                | players = (Player (List.length model.players) model.name 0) :: model.players
+                , name = ""
+            }
+
+        EditPlayer a ->
+            model
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "scoreboard" ]
+        [ h1 [] [ text "ScoreKeeper" ]
+        , playerForm model
+        , div [] [ text (toString model) ]
+        ]
+
+
+playerForm : Model -> Html Msg
+playerForm model =
+    Html.form [ onSubmit Save ]
+        [ input
+            [ type' "text"
+            , placeholder "Add/Edit Player"
+            , onInput Input
+            , value model.name
+            ]
+            []
+        , button [ type' "submit" ] [ text "Save" ]
+        , button [ type' "button", onClick Cancel ] [ text "Cancel" ]
+        ]
+
+
+main : Program Never
+main =
+    App.beginnerProgram { model = initModel, update = update, view = view }
