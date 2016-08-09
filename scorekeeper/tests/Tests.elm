95112674e8a4bb9fa1bd70c005f36305e05ced81
pnaranja <paul.naranja@gmail.com>
2016-08-08 18:34:33 -0700

    Create 3 player model Run elm-format on code

--- scorekeeper/tests/Tests.elm
+++ scorekeeper/tests/Tests.elm
@@ -5,16 +5,32 @@ import Expect
 import String
 import ScoreKeeper exposing (..)
 
+
 all : Test
 all =
     describe "A Test Suite"
         [ test "InitModel" <|
             \() -> initModelTest
-                
+
         , test "String.left" <|
             \() ->
                 Expect.equal "a" (String.left 1 "abcdefg")
         ]
 
+
 initModelTest : Expect.Expectation
-initModelTest = Expect.equal ScoreKeeper.initModel {players =[],name="",mode=AddPlayer,plays=[]}
+initModelTest =
+    Expect.equal ScoreKeeper.initModel { players = [], name = "", mode = AddPlayer, plays = [] }
+
+
+initThreePlayerModel : Model
+initThreePlayerModel =
+    { players =
+        [ { id = 2, name = "Joseph", totalpoints = 0, select = NotSelect }
+        , { id = 1, name = "Mark", totalpoints = 0, select = NotSelect }
+        , { id = 0, name = "Paul", totalpoints = 0, select = NotSelect }
+        ]
+    , name = ""
+    , mode = AddPlayer
+    , plays = []
+    }
